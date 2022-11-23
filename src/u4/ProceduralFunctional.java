de6f58650aa4c34f6bca9f08f159cb5892e6c9e6
Ostabo <osi.borkenhagen@gmail.com>
2022-11-23 10:26:25 +0100

    u4 more functional

--- src/u4/ProceduralFunctional.java
+++ src/u4/ProceduralFunctional.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.LinkedList;
 import java.util.List;
 
 public final class ProceduralFunctional {
@@ -14,33 +13,29 @@ public final class ProceduralFunctional {
 
     public static void main(String[] args) throws IOException {
         var input = Files.newBufferedReader(Paths.get(args[0]));
-        var lines = new LinkedList<String>();
 
         long start = System.nanoTime();
 
-        readLines(input, lines);
-        removeEmptyLines(lines);
-        removeShortLines(lines);
-        int n = totalLineLengths(lines);
+        var inList = readLines(input);
+        var emptiedList = removeEmptyLines(inList);
+        var shortenedList = removeShortLines(emptiedList);
+        int n = totalLineLengths(shortenedList);
 
         long stop = System.nanoTime();
 
         System.out.printf("result = %d (%d microsec)%n", n, (stop - start) / 1000);
     }
 
-    private static void readLines(BufferedReader input, List<String> lines) throws IOException {
-        String line;
-        while ((line = input.readLine()) != null) {
-            lines.add(line);
-        }
+    private static List<String> readLines(BufferedReader input) {
+        return input.lines().toList();
     }
 
-    private static void removeEmptyLines(List<String> lines) {
-        lines.removeIf(String::isEmpty);
+    private static List<String> removeEmptyLines(List<String> lines) {
+        return lines.stream().filter(s -> !s.isEmpty()).toList();
     }
 
-    private static void removeShortLines(List<String> lines) {
-        lines.removeIf(s -> s.length() < MIN_LENGTH);
+    private static List<String> removeShortLines(List<String> lines) {
+        return lines.stream().filter(s -> s.length() >= MIN_LENGTH).toList();
     }
 
     private static int totalLineLengths(List<String> lines) {
